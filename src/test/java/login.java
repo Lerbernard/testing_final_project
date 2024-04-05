4ef63212b54495ded268bc5a161ddfff598ad488
Lee  Bernard <68125142+Lerbernard@users.noreply.github.com>
2024-04-05 18:57:05 -0400

    fixing login and accessing the driver

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,27 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 
-
-public class login {
-
-
-
+public class login extends setup {
     @Test
-    void method1() throws InterruptedException {
-        WebDriver driver3 = new ChromeDriver();
-        driver3.manage().window().maximize();
-        driver3.quit();
-    }
+    void login_site(){
 
-    @Test
-    void method2() throws InterruptedException {
-        System.out.println("method 2");
-    }
-    @Test
-    void method3() throws InterruptedException {
-        System.out.println("method 3");
-    }
+        driver.get("https://www.reddit.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//*[@id=\'login-button\']")).click();
+        WebElement username = driver.findElement(By.xpath("/html/body/shreddit-app/shreddit-overlay-display/span[4]/input"));
+        WebElement password = driver.findElement(By.xpath("/html/body/shreddit-app/shreddit-overlay-display/span[5]/input"));
 
 
+        username.sendKeys("General_Baby24");
+        password.sendKeys("Testfinal24@");
+        password.sendKeys(Keys.ENTER);
+
+        }
 }
