320d31619828c07c6ff9572faf9451775a4df102
Lee  Bernard <68125142+Lerbernard@users.noreply.github.com>
2024-04-05 22:14:31 -0400

    add to creat_post

--- src/test/java/create_post.java
+++ src/test/java/create_post.java
@@ -1,19 +1,61 @@
 import org.openqa.selenium.By;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.*;
 
-public class create_post extends login{
+public class create_post extends setup_login {
+    public String url;
 
-    @BeforeTest
-    void  login()
-    {
+    @BeforeClass
+    void  login() throws InterruptedException {
         login_site();
+
+        url = driver.getCurrentUrl();
+        Thread.sleep(3000);
     }
+    @AfterClass
+    void close_driver(){ 
+        //driver.quit();
+    }
+
     @Test
     void create_a_post_with_text() throws InterruptedException {
-        Thread.sleep(5000);
+        driver.get(url);
+        Thread.sleep(3000);
+        //click the create post button
         driver.findElement(By.xpath("//*[@id=\'create-post\']/span")).click();
 
+
+        // adding text to the text field
+        driver.findElement(By.xpath("//*[@id=\'AppRouter-main-content\']/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[2]/div[1]/div/textarea")).sendKeys("testpost");
+        driver.findElement(By.xpath("//*[@id=\'AppRouter-main-content\']/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[2]/div[2]/div/div/div[3]/div/div[1]/div/div/div")).sendKeys("body");
+
+        // selecting where to post it
+        driver.findElement(By.xpath("//*[@id='AppRouter-main-content']/div/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[1]/input")).sendKeys("u/General_Baby24");
+        driver.findElement(By.xpath("//*[@id='AppRouter-main-content']/div/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[1]/input")).sendKeys(Keys.ENTER);
+
+        // remember to add click the submit bottom
+
+    }
+    @Test
+    void create_a_post_with_image() throws InterruptedException {
+        driver.get(url);
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//*[@id=\'create-post\']/span")).click();
+
+        // click image tab
+        driver.findElement(By.xpath("//*[@id=\'AppRouter-main-content\']/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[1]/div/button[2]")).click();
+
+        driver.findElement(By.xpath("//*[@id=\'AppRouter-main-content\']/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[2]/div[1]/div/textarea")).sendKeys("test");
+        WebElement image =  driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[2]/div[2]/div/div/input"));
+        image.sendKeys("C:\\Users\\leema\\OneDrive\\Documents\\CEN 4072 - CRN 15618 - Software Testing\\maven_testing\\src\\main\\resources\\test_image.gif");
+
+
+
+
+        driver.findElement(By.xpath("//*[@id='AppRouter-main-content']/div/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[1]/input")).sendKeys("u/General_Baby24");
+        driver.findElement(By.xpath("//*[@id='AppRouter-main-content']/div/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[1]/input")).sendKeys(Keys.ENTER);
 
 
     }
--- src/test/java/search.java
+++ src/test/java/search.java
@@ -4,17 +4,5 @@ import org.testng.annotations.Test;
 
 public class search {
 
-    @Test
-     void setup(){
-        WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.fgcu.edu/canvas/");
-        String title  = driver.getTitle();
-        System.out.println(title);
-
-        String url_page = driver.getCurrentUrl();
-        System.out.println(url_page);
-        driver.quit();
-
-    }
 }
