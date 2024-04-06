d23a70595bf47f2e0f33434a717b47e8d5f358e1
Lee  Bernard <68125142+Lerbernard@users.noreply.github.com>
2024-04-05 22:14:31 -0400

    fixing login

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,26 +1,8 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.openqa.selenium.WebDriver;
 
-public class login extends setup {
+public class login extends setup_login{
     @Test
-    void login_site(){
-
-        driver.get("https://www.reddit.com/");
-        driver.manage().window().maximize();
-
-        driver.findElement(By.xpath("//*[@id=\'login-button\']")).click();
-        WebElement username = driver.findElement(By.xpath("/html/body/shreddit-app/shreddit-overlay-display/span[4]/input"));
-        WebElement password = driver.findElement(By.xpath("/html/body/shreddit-app/shreddit-overlay-display/span[5]/input"));
-
-
-        username.sendKeys("General_Baby24");
-        password.sendKeys("Testfinal24@");
-        password.sendKeys(Keys.ENTER);
-
-        }
+    void  login_into_reddit() throws InterruptedException {
+        login_site();
+    }
 }
---
+++ src/test/java/setup_login.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class setup_login extends setup {
+    void login_site() throws InterruptedException {
+
+        driver.get("https://www.reddit.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//*[@id=\'login-button\']")).click();
+        Thread.sleep(5000);
+
+        WebElement username = driver.findElement(By.xpath("/html/body/shreddit-app/shreddit-overlay-display/span[4]/input"));
+        WebElement password = driver.findElement(By.xpath("/html/body/shreddit-app/shreddit-overlay-display/span[5]/input"));
+
+
+        username.sendKeys("General_Baby24");
+        password.sendKeys("Testfinal24@");
+
+        password.sendKeys(Keys.ENTER);
+        Thread.sleep(5000);
+
+    }
+}
