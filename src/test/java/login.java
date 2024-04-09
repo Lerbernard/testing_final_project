0aed417ff89e859f5c9eae7ef7750e3f2095efd4
Lee  Bernard <68125142+Lerbernard@users.noreply.github.com>
2024-04-08 23:24:39 -0400

    Merge remote-tracking branch 'origin/lee' into lee

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,8 +1,10 @@
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class login extends setup_login{
     @Test
     void  login_into_reddit() throws InterruptedException {
         login_site();
+        driver.quit();
     }
 }
