dec84b623bbf351ded939113a8b8a312ac4043a7
Lee  Bernard <68125142+Lerbernard@users.noreply.github.com>
2024-04-11 19:06:39 -0400

    Merge remote-tracking branch 'origin/lee' into lee

--- src/test/java/search.java
+++ src/test/java/search.java
@@ -1,8 +1,42 @@
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class search {
+public class search extends setup_login {
+    public String url;
 
 
+    @BeforeClass
+    void  login() throws InterruptedException {
+        login_site();
+
+        url = driver.getCurrentUrl();
+        Thread.sleep(3000);
+    }
+
+    @AfterClass
+    void close_driver(){
+        driver.quit();
+    }
+
+    @Test
+    void seerch_page() throws InterruptedException {
+
+        driver.get(url);
+        Thread.sleep(3000);
+
+        Actions search = new Actions(driver);
+        search.moveToLocation(966, 31).click().build().perform();
+        search.moveToLocation(966, 31).sendKeys("test").build().perform();
+        Thread.sleep(3000);
+        search.moveToLocation(966, 31).sendKeys(Keys.ENTER).build().perform();
+        Thread.sleep(3000);
+        
+        
+
+    }
 }
--- src/test/java/subreddit_page.java
+++ src/test/java/subreddit_page.java
@@ -1,2 +1,50 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
 public class subreddit_page {
+
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeTest
+    public void setUp() throws InterruptedException {
+        driver.get("https://www.reddit.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        //get the searchbar and enter keys
+        WebElement searchbar = driver.findElement(By.xpath("//reddit-search-large"));
+        searchbar.click();
+        searchbar.sendKeys("r/SoftwareTesting");
+        Thread.sleep(2000);
+
+        Actions action = new Actions(driver);
+        action.moveToLocation(606,114).click().perform();   //clicking the subreddit page
+    }
+    @Test
+    public void joinTheSubreddit() throws InterruptedException {
+        //joining the subreddit
+        Actions join = new Actions(driver);
+        join.moveToLocation(1400,159).click().perform();
+        Thread.sleep(2000);
+    }
+    @Test
+    public void clickPosts() throws InterruptedException {
+        Actions newPosts = new Actions(driver);
+        newPosts.moveToLocation(390,358).click().perform();
+    }
+
+
+    @AfterTest
+    public void tearDown2() throws InterruptedException {
+        driver.quit();
+    }
 }
